cf778efcb05e5cbfb70a34c19d6a390f6722185c
Onur Eker <yonureker@gmail.com>
2018-01-16 23:50:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,9 +17,10 @@ class Artist
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    add_song(song)
+    # @songs << song
+    # song.artist = self
+    # @@song_count += 1
   end
 
   def self.song_count
