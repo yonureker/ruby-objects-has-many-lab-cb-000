1b06d3be2a14d5d9a6837b4928cebc32f3aa62d6
Onur Eker <yonureker@gmail.com>
2018-01-16 23:56:10 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,16 @@
 class Post
+
+  attr_accessor :author, :title
+
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-  
-  def name
-    @name
-  end
-
-  def artist
+  def author_name
+    if self.author == nil
+      nil
+    else
+      self.author.name
+    end
   end
 end
