35f92e600a650b42b8c9949f9af5102966695895
Onur Eker <yonureker@gmail.com>
2018-01-16 23:50:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,16 @@
 class Song
+  attr_accessor :artist, :name
+
   def initialize(name)
     @name = name
   end
 
-  def name
-    @name
-  end
-
-  def artist
-  end
-
   def artist_name
-
+    if self.artist == nil
+      nil
+    else
+      self.artist.name
+    end
   end
 
 end
